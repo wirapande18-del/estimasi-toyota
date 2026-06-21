1bcb5f063d4de38cfd4e91a35af87bdc35f89151
Wira Pande <wirapande18@gmail.com>
2026-06-21 21:27:33 +0800

    update mobile autocomplete harga

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App_urut_part_sublet_jasa.jsx'
+import App from './App_mobile_autocomplete_update_harga.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
