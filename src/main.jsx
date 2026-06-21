3e18b44251a9007ba044e2bcd8e009f26b37bf51
Wira Pande <wirapande18@gmail.com>
2026-06-21 18:53:36 +0800

    perbaikan estimasi

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App(9).jsx'
+import App from './App_urut_part_sublet_jasa.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
