f46af03d3e04d9b6ef93f502f7769adb88f1b896
Wira Pande <wirapande18@gmail.com>
2026-06-21 22:30:05 +0800

    balik posisi sublet dan jasa

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App_sublet_diatas_jasa.jsx'
+import App from './App_kolom_catatan_pdf.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
