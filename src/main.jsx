76c860a4902a841b2f562f053f2b67c8cfd693bb
Wira Pande <wirapande18@gmail.com>
2026-06-22 19:42:43 +0800

    update sparepart dan master jasa

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App_kolom_catatan_pdf.jsx'
+import App from './App_perbaikan_update_harga.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
